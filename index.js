da572924ef9ab1cd161df8cdd66f50c877ade63a
gosuego <56798016+gosuego@users.noreply.github.com>
2020-10-24 01:41:30 +0900

    object Destructuring

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const human = {
     name: "Nico",
     lastName: "Serrano",
-     nationality: "Wish i was korean"
+     nationality: "Wish i was korean",
+     favFood: {
+         breakfast: "grape",
+         lunch: "meat",
+         dinner:"cheeze"
+
+     }
 }
+
+const name= human.name;
+
+const {
+    name,
+    lastName,
+    nationality: difName,
+    favFood: {dinner, breakfast, lunch}
+} = human;
+
+console.log(name, lastName, difName, dinner, breakfast, lunch);
