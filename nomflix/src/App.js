6c5de773fb05fa923e4863093673fe7f4e02490c
gosuego <56798016+gosuego@users.noreply.github.com>
2020-10-24 10:49:16 +0900

    [refactor]: Strat to clean

---
+++ nomflix/src/App.js
@@ -0,0 +1,12 @@
+
+import { render } from "@testing-library/react";
+import React, {Component} from "react";
+
+class App extends Component {
+  render() {
+   return <div className="App"/>;
+      
+}
+}
+
+export default App;
